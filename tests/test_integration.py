a6fc884ca5a221df6413496e7273ec4ddcdd5dba
Laurie O <laurie_opperman@hotmail.com>
2020-03-21 01:12:51 +1000

    Add more integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,6 +6,7 @@ import subprocess as sp
 
 import proxpi
 import pytest
+import requests
 from werkzeug import serving as werkzeug_serving
 
 lg.root.setLevel(lg.DEBUG)
@@ -21,7 +22,7 @@ class Thread(threading.Thread):
             self.exc = e
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def server():
     server = werkzeug_serving.make_server(
         host="127.0.0.1", port=5042, app=proxpi.app
@@ -55,3 +56,42 @@ def test_pip_download(server, tmp_path):
     contents = list((tmp_path / "dest2").iterdir())
     print(contents)
     assert any("jinja2" in p.name.lower() for p in contents)
+
+
+def test_list(server):
+    """Test getting package list."""
+    response = requests.get("http://127.0.0.1:5042/index/")
+    assert response.status_code == 200
+    assert "simplejson" in response.text
+
+
+def test_invalidate_list(server):
+    """Test invalidating package list cache."""
+    response = requests.delete("http://127.0.0.1:5042/cache/list")
+    assert response.status_code == 200
+    assert response.json() == {"status": "success", "data": None}
+
+
+def test_invalidate_package(server):
+    """Test invalidating package list cache."""
+    response = requests.delete("http://127.0.0.1:5042/cache/jinja2")
+    assert response.status_code == 200
+    assert response.json() == {"status": "success", "data": None}
+
+
+def test_nonexistant_package(server):
+    """Test getting non-existant package file list."""
+    response = requests.get("http://127.0.0.1:5042/index/ultraspampackage/")
+    assert response.status_code == 404
+
+
+def test_nonexistant_file(server):
+    """Test getting non-existant package file."""
+    response = requests.get("http://127.0.0.1:5042/index/ultraspampackage/spam.whl")
+    assert response.status_code == 404
+
+
+def test_nonexistant_file_from_existing_package(server):
+    """Test getting non-existant package file from existing package."""
+    response = requests.get("http://127.0.0.1:5042/index/Jinja2/nonexistant.whl")
+    assert response.status_code == 404
