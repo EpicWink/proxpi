d53f12b89863eeab320d8fb7581c27a397a935b2
Laurie O <laurie_opperman@hotmail.com>
2022-06-02 16:09:02 +1000

    Add JSON API benchmark script

---
+++ scripts/benchmark-json-api-response-size.py
@@ -0,0 +1,50 @@
+"""Benchmark JSON API response size again HTML API.
+
+Note: ``requests`` by default asks for gzip/zlib compression.
+"""
+
+import re
+import sys
+import json
+
+import requests
+
+normalise_pattern = re.compile(r"[^a-z\d-]+")
+
+packages_json = sys.stdin.read()
+package_names = sorted(set(p["name"] for p in json.loads(packages_json)))
+
+print("| Project | HTML size (kB) | JSON size (kB) | JSON size ratio |")
+print("| ------- | -------------- | -------------- | --------------- |")
+ratios = []
+for package_name in package_names:
+    package_name = normalise_pattern.sub("-", package_name.lower())
+
+    response = requests.get(
+        f"http://localhost:5042/index/{package_name}/", headers={"Accept": "text/html"}
+    )
+    if not response.ok:
+        print(
+            f"Failed '{package_name}: [{response.status_code}] {response.reason}",
+            file=sys.stderr,
+        )
+        continue
+    assert response.headers["Content-Encoding"] in ("gzip", "deflate")
+    html_length = round(int(response.headers["Content-Length"]) / 1024, 1)
+
+    response = requests.get(
+        f"http://localhost:5042/index/{package_name}/", headers={
+            "Accept": "application/vnd.pypi.simple.latest+json",
+        }
+    )
+    assert response.headers["Content-Encoding"] in ("gzip", "deflate")
+    response.raise_for_status()
+    json_length = round(int(response.headers["Content-Length"]) / 1024, 1)
+
+    ratios.append(json_length / html_length)
+    ratio = round(ratios[-1], 2)
+    print(f"| {package_name} | {html_length} | {json_length} | {ratio} |")
+
+mean_ratio = sum(ratios) / len(ratios)
+ratio_stddev = (sum((r - mean_ratio) ** 2.0 for r in ratios) / len(ratios)) ** 0.5
+print(f"\nAverage ratio: {mean_ratio} ± {ratio_stddev}")
