79ee8fa3c5c3d62a2e33d9795cefcd34dc3a8d04
Laurie O <laurie_opperman@hotmail.com>
2026-04-27 19:24:31 +1000

    Add pip test

--- tests/test_pypi.py
+++ tests/test_pypi.py
@@ -19,7 +19,16 @@ def server():
     yield from _utils.make_server(proxpi.server.app)
 
 
-def test_pip_download(server, tmp_path):
+@pytest.mark.parametrize(("uploaded_prior_to", "expected_jinja2_version"), [
+    pytest.param(None, None, id="latest"),
+    pytest.param("2025-01-01", "3.1.5", id="date_bound"),
+])  # fmt: skip
+def test_pip_download(
+    server,
+    tmp_path,
+    uploaded_prior_to: str,
+    expected_jinja2_version: str,
+) -> None:
     """Test package installation."""
     args = [
         sys.executable,
@@ -30,6 +39,21 @@ def test_pip_download(server, tmp_path):
         "--index-url", f"{server}/index/",
     ]
 
+    if uploaded_prior_to:
+        p_v = subprocess.run(
+            [sys.executable, "-m", "pip", "--version"],
+            check=True,
+            stdout=subprocess.PIPE,
+        )
+        pip_version = tuple(
+            int(x)
+            for x in p_v.stdout.decode(encoding="utf-8").split()[1].split(".")[:2]
+        )
+        if pip_version < (26, 1):
+            pytest.skip(reason="pip v26.1+ required for date-bound test")
+
+        args.extend(["--uploaded-prior-to", uploaded_prior_to])
+
     p = subprocess.run(
         [*args, "--dest", str(tmp_path / "dest1"), "Jinja2", "marshmallow"],
     )
@@ -39,7 +63,14 @@ def test_pip_download(server, tmp_path):
     assert any("jinja2" in p.name.lower() for p in contents)
     assert any("marshmallow" in p.name.lower() for p in contents)
 
-    subprocess.run([*args, "--dest", str(tmp_path / "dest2"), "Jinja2"])
+    if expected_jinja2_version:
+        filenames = set(p.name.lower() for p in contents)
+        assert (
+            any(f"jinja2-{expected_jinja2_version}" in f for f in filenames)
+            or any(f"jinja2_{expected_jinja2_version}" in f for f in filenames)
+        )
+
+    p = subprocess.run([*args, "--dest", str(tmp_path / "dest2"), "Jinja2"])
     assert p.returncode == 0
     contents = list((tmp_path / "dest2").iterdir())
     print(contents)
